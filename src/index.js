02f6b17fae2d9d259c6fba9f99985749d39db447
maystar236 <70463399+maystar236@users.noreply.github.com>
2024-07-26 21:42:46 +1000

    v1 of CSS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 function greeting() {
   let greeting = document.querySelector("#hello");
   greeting.innerHTML = "Hi, G'day, Ni hao, Hey,  Bonjour";}
-  setTimeout(greeting, 2000);
+  setTimeout(greeting, 3000);
 
   let localTime = moment.tz.guess();
   let localElement = document.querySelector("#local");
-  localElement.innerHTML = `Your current time zone is ${localTime} and the current time is ${moment().format("h:m A")}`;
+  localElement.innerHTML = `Your current time zone is ${localTime} . <br> It is ${moment().format("dddd, MMMM D, YYYY")} and the time is ${moment().format("h:m A")}`;
 
 function showSelectedCity(event) {
   if (event.target.value.length > 0) {
-  let currentTime = moment().tz(event.target.value).format("dddd, MMMM D, YYYY h:m A");
+  let currentTime = moment().tz(event.target.value).format("dddd, MMMM D, YYYY hh:mm A");
   alert(`It is ${currentTime} in ${event.target.value}`);}}
         
   let selectElement = document.querySelector("#cities");
